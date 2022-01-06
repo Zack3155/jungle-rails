8044f016aa1329c2e2dd0b41b478cc51eb75069e
Zack3155 <zhanbin3155@gmail.com>
2022-01-05 21:47:11 -0500

    Add Feature: Admin Dashboard - Counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['BASIC_AUTH_ACC'], password: ENV['BASIC_AUTH_PW']
   
   def show
+    @product_count = Product.all.size
+    @category_count = Category.all.size
   end
 end
